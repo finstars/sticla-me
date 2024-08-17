5271d54554c9c802b15ab9d2d187ba5f7cf90640
Fin <finstars@proton.me>
2024-08-18 00:08:31 +0300

    changes

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import AutoPlayAudio from "./AutoPlayAudio";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <h1 className={styles.title}><span>sticla</span>.me</h1>
       <p className={styles.description}>Don't Bottle Up, Recycle Smart</p>
       <a href="https://docs.google.com/forms/d/e/1FAIpQLSfhQuQBESXXreeSkUnwmTRTdWyVk78GiOP17pL72_ID9kiqDQ/viewform?usp=sf_link" target="_blank" className={styles.joinWaitlistButton}>Join Waitlist</a>
+      <AutoPlayAudio />
     </div>
   );
 }
