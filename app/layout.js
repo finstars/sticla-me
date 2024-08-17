42e181fc2b13751ef95f776f404a142691f27fce
Fin <finstars@proton.me>
2024-08-18 00:15:05 +0300

    changes

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import { Poppins } from "next/font/google";
 import "./bg.scss";
 import "./globals.css";
 
-const noto = Poppins({ weight: ["400", "500", "600", "700", "800", "900"], subsets: ["latin"] })
+const noto = Poppins({ weight: ["400", "500", "600", "700", "800", "900"], subsets: ["latin-ext"] })
 
 export const metadata = {
   title: "Sticla.me",
