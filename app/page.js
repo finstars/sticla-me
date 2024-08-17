83579b44b9e9e2cb248aacea9b08e5aaa026df81
Fin <finstars@proton.me>
2024-08-17 22:41:08 +0300

    changes

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.homepage}>
       {/* <h3>Sticla</h3> */}
-      <h1 className={styles.title}><span>Sticla</span>.me</h1>
+      <h1 className={styles.title}><span>sticla</span>.me</h1>
       <p className={styles.description}>Don't Bottle Up, Recycle Smart</p>
       <a href="https://docs.google.com/forms/d/e/1FAIpQLSfhQuQBESXXreeSkUnwmTRTdWyVk78GiOP17pL72_ID9kiqDQ/viewform?usp=sf_link" target="_blank" className={styles.joinWaitlistButton}>Join Waitlist</a>
     </div>
